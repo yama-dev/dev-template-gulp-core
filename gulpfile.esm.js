9c0d160deaa8c01410e4b65e548b3682ec43849a
yama-dev <tatsuya.yamamoto69@gmail.com>
2024-01-29 21:24:20 +0900

    v1.14.1

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -12,24 +12,23 @@ let _copy = `${pkg.name.toUpperCase()}
 console.log('-'.repeat(38) + '\n'+ _copy + '\n'+'-'.repeat(38));
 import { series, parallel } from 'gulp';
 import taskSass from './.dev/sass/index.js';
-import {
-  taskJsBabel,
-  taskJsWebpack,
-} from './.dev/javascript_babel/index.js';
+import taskJsBabel from './.dev/javascript_babel/index.js';
+import taskWebpack from './.dev/webpack/index.js';
 import taskTemplate from './.dev/template/index.js';
 import taskServer from './.dev/server/index.js';
 import taskWatch from './.dev/watch/index.js';
 import taskClean from './.dev/clean/index.js';
 import taskPhp from './.dev/php/index.js';
 import taskTwig from './.dev/twig/index.js';
+import taskCopy from './.dev/copy/index.js';
 import tackDeploy from './.dev/deploy/index.js';
 export default series(
   taskClean,
   taskTemplate,
   taskSass,
   taskJsBabel,
-  taskJsWebpack,
   parallel(
+    taskWebpack,
     taskPhp,
     taskTwig,
     taskServer,
@@ -41,19 +40,27 @@ export const prod = series(
   taskTemplate,
   taskSass,
   taskJsBabel,
-  taskJsWebpack,
   parallel(
+    taskWebpack,
     taskPhp,
     taskTwig,
     taskServer,
     taskWatch
   )
 );
+export const build = series(
+  taskClean,
+  taskTemplate,
+  taskSass,
+  taskJsBabel,
+  taskWebpack,
+  taskCopy
+);
 export const deploy = series(
   taskClean,
   taskTemplate,
   taskSass,
   taskJsBabel,
-  taskJsWebpack,
+  taskWebpack,
   tackDeploy
 );
